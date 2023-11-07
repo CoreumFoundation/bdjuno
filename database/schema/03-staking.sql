d451238da1003647690ac89117e348b0d90ea098
Dzmitry Hil <dzmitryhil@gmail.com>
2023-11-07 17:16:10 +0300

    Rename 14-assetnftparams.sql to 15-assetnftparams.sql

--- database/schema/03-staking.sql
+++ database/schema/03-staking.sql
@@ -26,6 +26,7 @@ CREATE INDEX staking_pool_height_index ON staking_pool (height);
 CREATE TABLE validator_info
 (
     consensus_address     TEXT   NOT NULL UNIQUE PRIMARY KEY REFERENCES validator (consensus_address),
+    --  TODO check that removal of operator_address UNIQUE modifier doesn't break new migration
     operator_address      TEXT   NOT NULL,
     self_delegate_address TEXT REFERENCES account (address),
     max_change_rate       TEXT   NOT NULL,
