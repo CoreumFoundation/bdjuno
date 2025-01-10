a6863c1b67892b5bed792051739ebaea0fa6452d
Dzmitry Hil <dzmitryhil@gmail.com>
2025-01-10 13:59:08 +0300

    Use callisto builder to build and publish docker images.

--- build/callisto/images.go
+++ build/callisto/images.go
@@ -11,22 +11,43 @@ import (
 	"github.com/CoreumFoundation/crust/build/types"
 )
 
-// BuildDockerImage builds docker image of the faucet.
+// BuildDockerImage builds docker image.
 func BuildDockerImage(ctx context.Context, deps types.DepsFunc) error {
+	return buildDockerImage(ctx, deps, false)
+}
+
+// ReleaseDockerImage releases docker image.
+func ReleaseDockerImage(ctx context.Context, deps types.DepsFunc) error {
+	return buildDockerImage(ctx, deps, true)
+}
+
+func buildDockerImage(ctx context.Context, deps types.DepsFunc, push bool) error {
 	deps(Build)
 
 	dockerfile, err := image.Execute(image.Data{
-		From:   docker.AlpineImage,
-		Binary: binaryPath,
+		From:         docker.AlpineImage,
+		BinaryPath:   filepath.Join("bin", ".cache", binaryName, tools.TargetPlatformLinuxLocalArchInDocker.String(), "bin", binaryName),
+		BinaryName:   binaryName,
+		DBSchemaPath: filepath.Join("database", "schema"),
 	})
 	if err != nil {
 		return err
 	}
 
+	var action docker.Action
+	if push {
+		action = docker.ActionPush
+	} else {
+		action = docker.ActionLoad
+	}
+
 	return docker.BuildImage(ctx, docker.BuildImageConfig{
-		ContextDir: filepath.Join("bin", ".cache", binaryName, tools.TargetPlatformLinuxLocalArchInDocker.String()),
-		ImageName:  binaryName,
+		ContextDir: ".",
+		ImageName:  config.DockerHubUsername + "/" + binaryName,
 		Dockerfile: dockerfile,
-		Versions:   []string{config.ZNetVersion},
+		Action:     action,
+		Versions: []string{
+			"latest",
+		},
 	})
 }
--- build/hasura/images.go
+++ build/hasura/images.go
@@ -12,12 +12,30 @@ import (
 //go:embed Dockerfile
 var dockerfile []byte
 
-// BuildDockerImage builds docker image of the faucet.
+// BuildDockerImage builds docker image.
 func BuildDockerImage(ctx context.Context, deps types.DepsFunc) error {
+	return buildDockerImage(ctx, false)
+}
+
+// ReleaseDockerImage builds and releases docker image.
+func ReleaseDockerImage(ctx context.Context, deps types.DepsFunc) error {
+	return buildDockerImage(ctx, true)
+}
+
+func buildDockerImage(ctx context.Context, push bool) error {
+	var action docker.Action
+	if push {
+		action = docker.ActionPush
+	} else {
+		action = docker.ActionLoad
+	}
 	return docker.BuildImage(ctx, docker.BuildImageConfig{
 		ContextDir: ".", // TODO (wojciech): Later on, move `hasura` dir here
-		ImageName:  "hasura",
+		ImageName:  config.DockerHubUsername + "/hasura",
 		Dockerfile: dockerfile,
-		Versions:   []string{config.ZNetVersion},
+		Action:     action,
+		Versions: []string{
+			"latest",
+		},
 	})
 }
